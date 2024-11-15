15430089581c185fb44793f9dc1ac208e279efd7
Juan Camilo <jalbaranmontoya@gmail.com>
2024-11-15 14:55:02 -0500

    Configuracion para despliegue del backend

--- src/main/java/co/edu/uniquindio/unieventos/configs/FirebaseConfig.java
+++ src/main/java/co/edu/uniquindio/unieventos/configs/FirebaseConfig.java
@@ -5,18 +5,19 @@ import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
 
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 
 @Configuration
 public class FirebaseConfig {
 
     @Bean
     public FirebaseApp intializeFirebase() throws IOException {
-        FileInputStream serviceAccount = new FileInputStream(
-                "src/main/resources/unievento-90431-firebase-adminsdk-pwxx0-66a82bf5f1.json"
-        );
+        InputStream serviceAccount = new ClassPathResource("unievento-90431-firebase-adminsdk-pwxx0-66a82bf5f1.json").getInputStream();
+
 
         FirebaseOptions options = FirebaseOptions.builder()
                 .setCredentials(GoogleCredentials.fromStream(serviceAccount))
